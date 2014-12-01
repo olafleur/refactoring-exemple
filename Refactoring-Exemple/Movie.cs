1196d115da7212bcd966f474d65001dbdf3d19ce
Olivier Lafleur <olivier.lafleur@gmail.com>
2014-11-30 21:17:59 -0500

    Move Method

--- Refactoring-Exemple/Movie.cs
+++ Refactoring-Exemple/Movie.cs
@@ -29,5 +29,30 @@
         {
             return _title;
         }
+
+        public double GetCharge(int daysRented)
+        {
+            double result = 0;
+            switch (GetPriceCode())
+            {
+                case Movie.Regular:
+                    result += 2;
+                    if (daysRented > 2)
+                    {
+                        result += (daysRented - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NewRelease:
+                    result += daysRented * 3;
+                    break;
+                case Movie.Childrens:
+                    result += 1.5;
+                    if (daysRented > 3)
+                        result += (daysRented - 3) * 1.5;
+                    break;
+            }
+
+            return result;
+        }
     }
 }
