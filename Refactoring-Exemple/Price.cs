3f48e8b89140db640ae877c405536d523811e856
Olivier Lafleur <olivier.lafleur@gmail.com>
2014-11-30 22:03:02 -0500

    Move Method

--- Refactoring-Exemple/Price.cs
+++ Refactoring-Exemple/Price.cs
@@ -9,6 +9,31 @@ namespace Refactoring_Exemple
     abstract class Price
     {
         public abstract int GetPriceCode();
+
+        public double GetCharge(int daysRented)
+        {
+            double result = 0;
+            switch (GetPriceCode())
+            {
+                case Movie.Regular:
+                    result += 2;
+                    if (daysRented > 2)
+                    {
+                        result += (daysRented - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NewRelease:
+                    result += daysRented * 3;
+                    break;
+                case Movie.Childrens:
+                    result += 1.5;
+                    if (daysRented > 3)
+                        result += (daysRented - 3) * 1.5;
+                    break;
+            }
+
+            return result;
+        }
     }
 
     class ChildrensPrice : Price
